d73660f6a4e13343b5b4784a6719e676807d579b
unknown <maxwellwong91@gmail.com>
2020-08-06 17:23:54 -0700

    updated server

--- website/backend/server.js
+++ website/backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var cors = require("cors");
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 // connect database
 const initializeDB = require("./src/database");
