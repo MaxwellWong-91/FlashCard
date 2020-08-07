b1bf2c1ea18af6062ad39ce0b4fa140441e267a2
unknown <maxwellwong91@gmail.com>
2020-08-06 18:01:10 -0700

    removed name

--- website/frontend/src/components/Navbar.js
+++ website/frontend/src/components/Navbar.js
@@ -31,7 +31,7 @@ function Navbar({history}) {
   const handleLogout = (e) => {
     setUser(null);
     localStorage.removeItem("token");
-    localStorage.removeItem("name");
+    localStorage.removeItem("username");
   }
 
   useEffect(() => {
